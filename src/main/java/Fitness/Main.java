2509303a5cb2618a3898e9c40bd8de9dd6d6540a
ibrahim <ibrahimmashaqi04@gmail.com>
2024-12-02 13:55:53 +0200

    new main

--- src/main/java/Fitness/Main.java
+++ src/main/java/Fitness/Main.java
@@ -3,33 +3,33 @@ package Fitness;
 public class Main {
     public static void main(String[] args) {
 
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-        Application.users.add(new User());
-
-
-       User x[]=new Admin().displayEngagedUsers();
-        System.out.println("/////////////////////////////////////////////");
-       for(User u : x){
-           if(u==null) System.out.println("null");
-           System.out.println(u.getPoints());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//        Application.users.add(new User());
+//
+//
+//       User x[]=new Admin().displayEngagedUsers();
+//        System.out.println("/////////////////////////////////////////////");
+//       for(User u : x){
+//           if(u==null) System.out.println("null");
+//           System.out.println(u.getPoints());
        }
     }
-}
+
