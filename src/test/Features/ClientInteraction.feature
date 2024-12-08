4d3312d80fb80e0becf28b946c8f7e2a2ba16c62
Omar Abumazen <omarabumazen990@gmail.com>
2024-12-08 22:02:49 +0200

    feature2 - write & step2-part1

---
+++ src/test/Features/ClientInteraction.feature
@@ -0,0 +1,84 @@
+Feature: Client Interaction
+
+
+
+  Background:
+    Given an instructor with name "Malik"
+    And Malik has logged to the system
+    And client named "Ahmad"  has enrolled in a Malik's program
+    And client named "Sami"  has enrolled in a Malik's program
+    And client named "Emad"  has enrolled in a Malik's program
+
+
+
+ #first part
+
+    Scenario: Malik sends a message to a client
+      When Malik sends a message "Great job for today!" to Ahmad
+      Then Ahmad should receive in their inbox
+      And a notification must be sent to Ahmad's account
+
+    Scenario: Ahmad sends a message to Malik
+        Given Ahmad logged to the system
+        When Ahmad send a message to Malik "Hey instructor Malik, when should start training?
+        Then Malik should receive the message in his inbox
+        And the system should send a notification to Malik about the message
+
+    Scenario: Ahmad post a question in the discussion form
+        Given Ahmad logged to the system
+        When Ahmad post a question message "What is the best food for more power?" in the discussion form
+        Then the question message should display in the discussion form to the other clients and to the instructor Malik
+        And Malik should receive a notification about the new post
+
+    Scenario: Malik answer the clients questions on the discussion form
+          Given Ahmad asks a question "What is the most power?" in the discussion form
+          When Malik post the answer on the question
+          Then the answer should appear under the question in the discussion form
+          And Ahmad should receive a notification that there's a new answer to his question
+
+    Scenario: Other clients answer Ahmad's question
+            Given Sami has logged to the system
+            And Emad has logged to the system
+            And Ahmad post a question message "What is the best food for more power?" in the discussion form
+            When Sami answer Ahmad "Well, you have to practice more"
+            And Emad answer Ahmad "Well, as Sami said and you need to be patient!"
+            Then both answers should appear in the discussion form
+            And the owner question and every client has answered Ahmad should receive a message about the new answers
+
+    Scenario: there's a previous answer for a new question
+              Given Ahmad post a question message "What is the best food for more power?" in the discussion form
+              And Malik has already answered this question before
+              When Malik marks the question as answered before
+              Then the old answer should appear under the question
+              # with specifc date and time for each answer like facebook!!
+              And the system should sent notification to all participants in the Ahmad's question
+
+#second part------------------------
+
+
+    Scenario: Malik provides feedback or reports
+      Given Malik creates a feedback or reports for Ahmad
+      When Malik sent the results to Ahmad
+      And Malik can write a comment about Ahmad's results
+      Then the reports should be available in the client's progress section
+      And the system should display Malik's comment about Ahmad report
+      And the system should send a notification to Ahmad that receive a report or feedback from Malik
+
+
+
+      Scenario: Ahmad receive a report from Malik
+        Given Ahmad logged to the system
+        And receive a report from Malik
+        When Ahmad send a reply
+        Then the reply should appear to Malik in client's progress reports page
+        And the system should send a notification to Malik about Ahmad's reply
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/Fitness/ClientInteractionStep.java
@@ -0,0 +1,234 @@
+package Fitness;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ClientInteractionStep
+{
+
+    @Given("Malik has logged to the system")
+    public void malik_has_logged_to_the_system()
+    {
+        
+
+
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("client named {string}  has enrolled in a Malik's program")
+    public void client_named_has_enrolled_in_a_malik_s_program(String string) {
+
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("Malik sends a message {string} to Ahmad")
+    public void malik_sends_a_message_to_ahmad(String string) {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("Ahmad should receive in their inbox")
+    public void ahmad_should_receive_in_their_inbox() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("a notification must be sent to Ahmad's account")
+    public void a_notification_must_be_sent_to_ahmad_s_account() {
+
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("Ahmad logged to the system")
+    public void ahmad_logged_to_the_system() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("Ahmad send a message to Malik \"Hey instructor Malik, when should start training?")
+    public void ahmad_send_a_message_to_malik_hey_instructor_malik_when_should_start_training() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("Malik should receive the message in his inbox")
+    public void malik_should_receive_the_message_in_his_inbox() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the system should send a notification to Malik about the message")
+    public void the_system_should_send_a_notification_to_malik_about_the_message() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Ahmad post a question message {string} in the discussion form")
+    public void ahmad_post_a_question_message_in_the_discussion_form(String string) {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the question message should display in the discussion form to the other clients and to the instructor Malik")
+    public void the_question_message_should_display_in_the_discussion_form_to_the_other_clients_and_to_the_instructor_malik() {
+
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("Malik should receive a notification about the new post")
+    public void malik_should_receive_a_notification_about_the_new_post()
+    {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("Ahmad asks a question {string} in the discussion form")
+    public void ahmad_asks_a_question_in_the_discussion_form(String string) {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("Malik post the answer on the question")
+    public void malik_post_the_answer_on_the_question() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the answer should appear under the question in the discussion form")
+    public void the_answer_should_appear_under_the_question_in_the_discussion_form() {
+
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("Ahmad should receive a notification that there's a new answer to his question")
+    public void ahmad_should_receive_a_notification_that_there_s_a_new_answer_to_his_question() {
+
+       // throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("Sami has logged to the system")
+    public void sami_has_logged_to_the_system() {
+
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Given("Emad has logged to the system")
+    public void emad_has_logged_to_the_system() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Sami answer Ahmad {string}")
+    public void sami_answer_ahmad(String string) {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("Emad answer Ahmad {string}")
+    public void emad_answer_ahmad(String string) {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("both answers should appear in the discussion form")
+    public void both_answers_should_appear_in_the_discussion_form() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the owner question and every client has answered Ahmad should receive a message about the new answers")
+    public void the_owner_question_and_every_client_has_answered_ahmad_should_receive_a_message_about_the_new_answers() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Given("Malik has already answered this question before")
+    public void malik_has_already_answered_this_question_before() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("Malik marks the question as answered before")
+    public void malik_marks_the_question_as_answered_before() {
+
+
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("the old answer should appear under the question")
+    public void the_old_answer_should_appear_under_the_question() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the system should sent notification to all participants in the Ahmad's question")
+    public void the_system_should_sent_notification_to_all_participants_in_the_ahmad_s_question() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("Malik creates a feedback or reports for Ahmad")
+    public void malik_creates_a_feedback_or_reports_for_ahmad() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("Malik sent the results to Ahmad")
+    public void malik_sent_the_results_to_ahmad() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("Malik can write a comment about Ahmad's results")
+    public void malik_can_write_a_comment_about_ahmad_s_results() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the reports should be available in the client's progress section")
+    public void the_reports_should_be_available_in_the_client_progress_section() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the system should display Malik's comment about Ahmad report")
+    public void the_system_should_display_malik_s_comment_about_ahmad_report() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the system should send a notification to Ahmad that receive a report or feedback from Malik")
+    public void the_system_should_send_a_notification_to_ahmad_that_receive_a_report_or_feedback_from_malik() {
+
+       // throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Given("receive a report from Malik")
+    public void receive_a_report_from_malik() {
+
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("Ahmad send a reply")
+    public void ahmad_send_a_reply() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the reply should appear to Malik in client's progress reports page")
+    public void the_reply_should_appear_to_malik_in_client_s_progress_reports_page() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the system should send a notification to Malik about Ahmad's reply")
+    public void the_system_should_send_a_notification_to_malik_about_ahmad_s_reply() {
+
+        //throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+}
