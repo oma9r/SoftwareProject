dda3cf3d3cc5871e701d3184ddf720f88d091caa
Omar Abumazen <omarabumazen990@gmail.com>
2024-12-20 20:21:05 +0200

    fixing2

--- src/test/java/Fitness/UserManagementSteps.java
+++ src/test/java/Fitness/UserManagementSteps.java
@@ -1,6 +1,5 @@
-package use_cases;
+package Fitness;
 
-import Fitness.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -339,5 +338,6 @@ assertTrue(true);
     public void thenTheSystemDisplaysMessage(String expectedMessage) {
         assertEquals(expectedMessage, searchMessage);
     }
+
 
 }
